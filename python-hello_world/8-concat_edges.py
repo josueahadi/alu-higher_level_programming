9a2b54674631da23339e442cecc11b289bded3e6
Josue <h.ahadi1@alustudent.com>
2024-02-22 06:43:09 -0800

    The Zen of Python

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# Extract "object-oriented programming with Python" and print it
+print(str[39:66]+" with Python")
